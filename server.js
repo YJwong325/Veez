8549bb0c4e07bec03a03f31b1354f1b4bcd75008
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2025-12-29 17:20:28 -0500

    feat: test fetch API from AniList data set
     - add error pages

--- server.js
+++ server.js
@@ -8,12 +8,51 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/manga', (req, res) => {
-    res.render('manga')
+    let query = `
+        query ($id: Int) {
+            Media (id: $id, type: MANGA) {
+                id
+                title {
+                    english
+                }
+            }
+        }
+    `
+    let variables = {
+        id: 63327
+    }
+
+    let url = 'https://graphql.anilist.co'
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    })
+        .then((response) => {
+            return response.json().then((json) => {
+                return response.ok ? json : Promise.reject(json)
+            })
+        })
+        .then((data) => {
+            console.log(JSON.stringify(data))
+            res.render('manga', { mangas: data.data.Media })
+        })
+        .catch((err) => {
+            res.status(500).render('500', { errMessage: err })
+        })
 })
 
 app.get('/anime', (req, res) => {
@@ -22,6 +61,10 @@ app.get('/anime', (req, res) => {
 
 app.get('/manhwa', (req, res) => {
     res.render('manhwa')
+})
+
+app.use((req, res, next) => {
+    res.status(404).render('404')
 })
 
 app.listen(port, () => {
