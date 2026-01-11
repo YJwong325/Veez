7ef031b3d3531ed7a7c9e382eda9e44a2a66c794
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2026-01-11 17:26:39 -0500

    fix: initialize function to return a new Promise instead
     - change from concat to push data to media array

--- modules/media.js
+++ modules/media.js
@@ -4,128 +4,132 @@ const mediaData = require('../data/media.json')
 let media = []
 
 function initialize() {
-    let aniData = mediaData.filter((elem) => elem.cat_id === 1)
-    let mangaData = mediaData.filter((elem) => elem.cat_id !== 1)
-
-    // GraphQL query for fetching ANIME typed media
-    let query = `
-        query ($ids: [Int]) {
-            Page (perPage: 10) {
-                media (id_in: $ids, type: ANIME) {
-                    src_id: id
-                    title {
-                        english
+    return new Promise((resolve, reject) => {
+        let aniData = mediaData.filter((elem) => elem.cat_id === 1)
+        let mangaData = mediaData.filter((elem) => elem.cat_id !== 1)
+    
+        // GraphQL query for fetching ANIME typed media
+        let query = `
+            query ($ids: [Int]) {
+                Page (perPage: 10) {
+                    media (id_in: $ids, type: ANIME) {
+                        src_id: id
+                        title {
+                            english
+                        }
+                        coverImage {
+                            large
+                        }
+                        siteUrl
                     }
-                    coverImage {
-                        large
-                    }
-                    siteUrl
                 }
             }
+        `
+    
+        let variables = {
+            ids: []
         }
-    `
-
-    let variables = {
-        ids: []
-    }
-
-    aniData.forEach((anime) => {
-        variables.ids.push(anime.src_id)
-    })
-
-    let url = 'https://graphql.anilist.co'
-    let options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
+    
+        aniData.forEach((anime) => {
+            variables.ids.push(anime.src_id)
+        })
+    
+        let url = 'https://graphql.anilist.co'
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                query: query,
+                variables: variables
+            })
+        }
+    
+        // fetch ANIME
+        fetch(url, options)
+            .then((response) => {
+                return response.json().then((json) => {
+                    return response.ok ? json : Promise.reject(json)
+                })
+            })
+            .then((data) => {
+                // add id and cat_id fields to the fetched objects before appending to media array
+                // id acts as a primary key because src_id may not be unique
+                // the added id attribute can be used as primary key when we eventually add a DB
+                data.data.Page.media.forEach((obj) => {
+                    // all fetched objects in this query are type ANIME 
+                    obj.cat_id = 1
+                    
+                    // aniData is already filtered by category to make sure there are no
+                    // unexpected behavior with 2 objects sharing the same src_id in different categories
+                    let srcObj = aniData.find((elem) => elem.src_id === obj.src_id)
+                    obj.id = srcObj.id
+    
+                    let category = catData.find((elem) => elem.id === 1)
+                    obj.category = category.cat_name
+                })
+                media.push(...data.data.Page.media)
+            })
+            .catch((err) => {
+                reject(err)
+            })
+    
+        query = `
+            query ($ids: [Int]) {
+                Page (perPage: 10) {
+                    media (id_in: $ids, type: MANGA) {
+                        src_id: id
+                        title {
+                            english
+                        }
+                        coverImage {
+                            large
+                        }
+                        siteUrl
+                    }
+                }
+            }
+        `
+    
+        variables.ids = []
+    
+        mangaData.forEach((manga) => {
+            variables.ids.push(manga.src_id)
+        })
+    
+        // update request body for MANGA GraphQL query
+        options.body = JSON.stringify({
             query: query,
             variables: variables
         })
-    }
-
-    // fetch ANIME
-    fetch(url, options)
-        .then((response) => {
-            return response.json().then((json) => {
-                return response.ok ? json : Promise.reject(json)
+    
+        // fetch MANGA
+        fetch(url, options)
+            .then((response) => {
+                return response.json().then((json) => {
+                    return response.ok ? json : Promise.reject(json)
+                })
             })
-        })
-        .then((data) => {
-            // add id and cat_id fields to the fetched objects before appending to media array
-            // id acts as a primary key because src_id may not be unique
-            // the added id attribute can be used as primary key when we eventually add a DB
-            data.data.Page.media.forEach((obj) => {
-                // all fetched objects in this query are type ANIME 
-                obj.cat_id = 1
-                
-                // aniData is already filtered by category to make sure there are no
-                // unexpected behavior with 2 objects sharing the same src_id in different categories
-                let srcObj = aniData.find((elem) => elem.src_id === obj.src_id)
-                obj.id = srcObj.id
-
-                let category = catData.find((elem) => elem.id === 1)
-                obj.category = category.cat_name
+            .then((data) => {
+                data.data.Page.media.forEach((obj) => {
+                    let srcObj = mangaData.find((elem) => elem.src_id === obj.src_id)
+    
+                    obj.cat_id = srcObj.cat_id
+                    obj.id = srcObj.id
+                    
+                    let category = catData.find((elem) => elem.id === srcObj.cat_id)
+                    obj.category = category.cat_name
+                })
+                media.push(...data.data.Page.media)
             })
-            media.concat(data.data.Page.media)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            .catch((err) => {
+                reject(err)
+            })
 
-    query = `
-        query ($ids: [Int]) {
-            Page (perPage: 10) {
-                media (id_in: $ids, type: MANGA) {
-                    src_id: id
-                    title {
-                        english
-                    }
-                    coverImage {
-                        large
-                    }
-                    siteUrl
-                }
-            }
-        }
-    `
-
-    variables.ids = []
-
-    mangaData.forEach((manga) => {
-        variables.ids.push(manga.src_id)
+        resolve()
     })
-
-    // update request body for MANGA GraphQL query
-    options.body = JSON.stringify({
-        query: query,
-        variables: variables
-    })
-
-    // fetch MANGA
-    fetch(url, options)
-        .then((response) => {
-            return response.json().then((json) => {
-                return response.ok ? json : Promise.reject(json)
-            })
-        })
-        .then((data) => {
-            data.data.Page.media.forEach((obj) => {
-                let srcObj = mangaData.find((elem) => elem.src_id === obj.src_id)
-
-                obj.cat_id = srcObj.cat_id
-                obj.id = srcObj.id
-                
-                let category = catData.find((elem) => elem.id === srcObj.cat_id)
-                obj.category = category.cat_name
-            })
-            media.concat(data.data.Page.media)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
 }
 
 function getAllMedia() {
