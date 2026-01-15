f20b09e05637df4bdc3b50763663f83d376c3034
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2026-01-15 16:26:59 -0500

    fix: change server.js to initialize before establishing routes

--- server.js
+++ server.js
@@ -13,43 +13,42 @@ app.set('views', path.join(__dirname, '/views'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('/', (req, res) => {
-    mediaData.initialize()
-        .then(() => {
-            mediaData.getAllMedia()
-                .then((data) => console.log(data));
+mediaData.initialize()
+    .then(() => {
+        app.get('/', (req, res) => {
+            res.render('home')
         })
-        .catch((err) => console.log(err));
         
-    res.render('home')
-})
-
-app.get('/manga', (req, res) => {
-    mediaData.getMediaById(20)
-        .then((data) => {
-            res.render('manga', { manga: data });
+        app.get('/manga', (req, res) => {
+            mediaData.getMediaById(20)
+                .then((data) => {
+                    res.render('manga', { manga: data });
+                })
+                .catch((err) => {
+                    res.status(500).json({ "message": "Internal server error" })
+                })
         })
-        .catch((err) => {
-            res.status(500).json({ "message": "Internal server error" })
+        
+        app.get('/media', (req, res) => {
+            res.render('media')
         })
-})
-
-app.get('/media', (req, res) => {
-    res.render('media')
-})
-
-app.get('/anime', (req, res) => {
-    res.render('anime')
-})
-
-app.get('/manhwa', (req, res) => {
-    res.render('manhwa')
-})
-
-app.use((req, res, next) => {
-    res.status(404).render('404')
-})
-
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`)
-})
+        
+        app.get('/anime', (req, res) => {
+            res.render('anime')
+        })
+        
+        app.get('/manhwa', (req, res) => {
+            res.render('manhwa')
+        })
+        
+        app.use((req, res, next) => {
+            res.status(404).render('404')
+        })
+        
+        app.listen(port, () => {
+            console.log(`Listening to port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
