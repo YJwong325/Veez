8b4bdaaabdc93e591b2553732c9b99e00b8034ed
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2026-02-12 23:13:20 -0500

    WIP: add description to individual media pages
     - descriptions are rendered using unescaped ejs tags

--- modules/media.js
+++ modules/media.js
@@ -18,6 +18,7 @@ function fetchMedia(type, filteredMedia) {
                         extraLarge
                     }
                     siteUrl
+                    description
                 }
             }
         }
